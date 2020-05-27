5473e85de6139370978feda6de63bb69dbbf9ab7
Anton Kovalenko <anton.kovalenko@fxclub.com>
2020-05-27 11:16:53 +0200

    hr

--- src/App.svelte
+++ src/App.svelte
@@ -22,25 +22,27 @@
 
 <div class="wrapper py-8 text-black">
   <div class="flex justify-between items-center mb-4">
-    <h1 class="text-2xl font-bold">Grape Cafe - Menu</h1>
+    <h1 class="text-2xl font-bold">Grape Cafe &mdash; Menu</h1>
     <div class="border-2 p-2 rounded-lg">РУ</div>
   </div>
   {#each $types as j}
     {#if $menu[j].length !== 0}
-      <div class="mb-12">
-        <h3 class="mb-2 text-xl font-bold">{$t[j][lang]}</h3>
+      <div class="pb-4">
+        <h3 class="mb-4 text-2xl font-bold">{$t[j][lang]}</h3>
         {#each $menu[j] as i}
           <div class="relative mb-4">
             <div
                 class="{i[itemIndex('available', $headers)] === 'TRUE' ? '' : ''} flex justify-between"
             >
               <div class="pr-2">
-                <div class="font-bold">{ i[itemIndex('name-' + lang, $headers)] }</div>
-                {#if i[itemIndex('available', $headers)] === 'FALSE'}
-                  <div class="inline-block py-1 px-4 bg-red-300 rounded-lg">
-                    <div class="">Закончилось!</div>
-                  </div>
-                {/if}
+                <div class="flex flex-wrap items-center">
+                  <div class="font-bold mr-1">{ i[itemIndex('name-' + lang, $headers)] }</div>
+                  {#if i[itemIndex('available', $headers)] === 'FALSE'}
+                    <div class="inline-block p-1 bg-red-300 rounded-lg text-sm">
+                      <div class="">Закончилось</div>
+                    </div>
+                  {/if}
+                </div>
                 {#if i[itemIndex('minimum', $headers)]}
                   <div class="text-sm text-gray-900">Минимальный заказ { i[itemIndex('minimum', $headers)] }г</div>
                 {/if}
@@ -56,6 +58,7 @@
           </div>
         {/each}
       </div>
+      <hr class="mb-6">
     {/if}
   {/each}
 </div>
